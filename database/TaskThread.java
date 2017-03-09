afed0ff4b4ea07e5d8deffcf34a0ee43f8a001b3
ar-sharma <sharma07arushi@gmail.com>
2017-03-09 18:37:30 +0530

    Add files via upload

--- database/TaskThread.java
+++ database/TaskThread.java
@@ -1,1 +1,63 @@
+package restservice.databse;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Random;
+
+
+
+public class TaskThread extends Thread {
+	
+	String name;
+	public TaskThread(){}
+	public TaskThread (String name) {
+		this.name = name;
+	}
+	boolean run = true;
+
+    public void run() {
+
+        Random rnd = new Random();
+       
+
+        try {
+        	
+            while(run == true) {
+
+                // ..
+                // Do whatever a client does ..
+                // ..
+
+                Integer sleepTime = rnd.nextInt(50000);
+
+             Thread.sleep(sleepTime);
+
+               Statement stmt = null;
+               Connection connection=DbConnection.connectToDatabase();
+     	      
+
+     		  System.out.println("Creating statement1...");
+     	      stmt = connection.createStatement();
+     	      String sql;
+     	      String sql2;
+     	     
+     	      sql = "UPDATE user1 SET task='2' where name='"+name+"'";
+     	      sql2 = "UPDATE user SET task='2' where name='"+name+"'";
+     	   int rowsUpdated1 = stmt.executeUpdate(sql);
+     	   int rowsUpdated2 = stmt.executeUpdate(sql2);
+     	    
+     	    if (rowsUpdated1>0 && rowsUpdated2>0) {
+     	    	run = false;
+     	    }
+            }
+
+        } catch(InterruptedException v) {
+            System.out.println(v);
+        } catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+    } 
+	
+}
