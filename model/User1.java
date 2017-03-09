3fec5684159968235d58b7cc4c4909b904a8c1b3
ar-sharma <sharma07arushi@gmail.com>
2017-03-09 20:51:56 +0530

    Add files via upload

---
+++ model/User1.java
@@ -0,0 +1,42 @@
+package restservice.model;
+
+public class User1 {
+
+	private int id;
+	private int task;
+	private String name;
+	private String start;
+	private String end;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getTask() {
+		return task;
+	}
+	public void setTask(int task) {
+		this.task = task;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getStart() {
+		return start;
+	}
+	public void setStart(String start) {
+		this.start = start;
+	}
+	public String getEnd() {
+		return end;
+	}
+	public void setEnd(String end) {
+		this.end = end;
+	}
+	
+	
+}
