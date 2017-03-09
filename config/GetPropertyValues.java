92164456831d75e26fcf6e4f0cfe0c67f6c148fd
ar-sharma <sharma07arushi@gmail.com>
2017-03-09 18:36:03 +0530

    Add files via upload

--- config/GetPropertyValues.java
+++ config/GetPropertyValues.java
@@ -1,1 +1,35 @@
+package restservice.config;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class GetPropertyValues {
+
+	String result="";
+	InputStream inputStream;
+	
+	public Properties getPropValues(String fileName) throws IOException {
+		
+		Properties prop = new Properties();
+		
+		try {
+			
+			//String propFileName = "config.properties";
+ 
+			//inputStream = getClass().getClassLoader().getResourceAsStream(propFileName);
+ 
+			InputStream inputStream = new FileInputStream(fileName);
+			 
+			if (inputStream != null) {
+				prop.load(inputStream);
+			}
+			
+} catch (Exception e) {
+	System.out.println("Exception: " + e);
+} 
+		return prop;
+		
+	}
+}
